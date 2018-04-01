1fa128cc2bb9ead20e86e2c9f59d04bbbe74243d
christowski <mail@christowski.de>
2018-04-01 11:23:45 +0200

    upload of initial landing page

---
+++ impressum.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+  <meta name="description" content="Innovation lässt sich spielen! Das Innovationsspiel Moonshot macht kreative Prozesse für Unternehmen und Startups spielbar und fördert mit zahlreichen Tools und Challenges ein adaptives Mindset." />
+  <meta name="author" content="Martin Wiens, Till hasbach" />
+  <meta name="keywords" content="Innovation, innovativ, Gamification, Playful Business, Innovationsspiel, Innovationsprozess, Moonshot, Kreativitätstechniken, Kreativität, Tools, Toolbox, Design Thinking, Startup, Workshop, Spiel, Unternehmen, Agilität, Mindset, adaptives Mindset, Brainstorming, brainstorm, game, disruptiv, Disruption, Kribbeln im Kopf, maker, Moon, Moonshot Thinking, Entrepreneur, Methodik, erfinden, Erfindung, Werkstatt, Knete, ludisch, Serendipität, Serendipity, Glück, Idee, Agentur, Proto, prototyp, Dragon Dreaming, Assoziation, Brainwriting, Open Innovation, Pitch" />
+
+  <meta property="og:url" content="www.innovationgame.de" />
+  <meta property="og:title" content="Moonshot – The Innovation Game" />
+  <meta property="og:description" content="Innovation lässt sich spielen! Das Innovationsspiel Moonshot macht kreative Prozesse für Unternehmen und Startups spielbar und fördert mit zahlreichen Tools und Challenges ein adaptives Mindset." />
+  <meta property="og:image" content="media/opengraph-image.png" />
+
+  <link rel="shortcut icon" href="media/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="media/apple-touch-icon.png">
+
+  <link rel="shortcut icon" href="media/favicon.ico" />
+  <title>MOONSHOT – Impressum</title>
+
+  <!-- STYLESHEETS -->
+  <link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+  <link rel="stylesheet" href="https://use.typekit.net/top2kgq.css">
+
+  <!-- JAVSCRIPT -->
+</head>
+
+<body>
+  <div id="wrapper">
+    <div class="container container-violet">
+      <h1 class="logo">
+        <a href="index.php"><img src="media/moonshot-logo.svg" alt="Moonshot Logo" style="max-width: 500px;" title="Moonshot – The Innovation Game"></a>
+        <!-- Hidden -->
+        <span>MOONSHOT – The Innovation Game</span>
+      </h1>
+      </div
+    </div>
+
+    <div class="container">
+
+      <div class="content">
+        <h2>Impressum</h2>
+        <p>Angaben gemäß § 5 TMG:<br>
+          PROTO GbR<br>
+          Mindener Str. 13<br>
+          10589 Berlin
+        </p>
+
+        <p>Vertreten durch:<br>
+          Till Hasbach, Martin Wiens<br>
+          <a title="Kontaktieren Sie uns" href="&#x6d;&#97;&#x69;&#x6c;&#116;&#x6f;&#58;&#104;&#101;&#108;&#108;&#111;&#x40;&#116;&#x68;&#101;&#105;&#110;&#110;&#x6f;&#118;&#x61;&#116;&#x69;&#x6f;&#110;&#103;&#x61;&#x6d;&#101;&#46;&#100;&#x65;">Kontaktieren Sie uns via Mail</a>.<br><br>
+        </p>
+
+        <p><strong>HAFTUNGSAUSSCHLUSS (DISCLAIMER)</strong></p>
+
+        <p><strong>Haftung für Inhalte</strong></p>
+        <p>ls Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.</p>
+
+        <p><strong>Haftung für Links</strong></p>
+        <p>Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.</p>
+
+        <p><strong>Urheberrecht</strong></p>
+        <p>Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.</p>
+
+        <p><strong>Google Analytics und Datenschutz</strong></p>
+        <p>Unsere Website nutzt Google Analytics, welches Nutzerdaten speichert und verarbeitet. Möchten Nutzer ihre Daten schützen, können sie beispielsweise über ein Browser-Add-on Google Analytics selbständig deaktivieren.</p>
+
+        <p><a href="index.php" title="Zurück zur Startseite">Zurück zur Startseite</a></p>
+      </div>
+
+    </div>
+
+    <footer>
+      <p>&copy; <?php echo date("Y"); ?> Martin Wiens, Till Hasbach.</p>
+    </footer>
+
+  </div> <!-- //#wrapper -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+  <meta name="description" content="Innovation lässt sich spielen! Das Innovationsspiel Moonshot macht kreative Prozesse für Unternehmen und Startups spielbar und fördert mit zahlreichen Tools und Challenges ein adaptives Mindset." />
+  <meta name="author" content="Martin Wiens, Till hasbach" />
+  <meta name="keywords" content="Innovation, innovativ, Gamification, Playful Business, Innovationsspiel, Innovationsprozess, Moonshot, Kreativitätstechniken, Kreativität, Tools, Toolbox, Design Thinking, Startup, Workshop, Spiel, Unternehmen, Agilität, Mindset, adaptives Mindset, Brainstorming, brainstorm, game, disruptiv, Disruption, Kribbeln im Kopf, maker, Moon, Moonshot Thinking, Entrepreneur, Methodik, erfinden, Erfindung, Werkstatt, Knete, ludisch, Serendipität, Serendipity, Glück, Idee, Agentur, Proto, prototyp, Dragon Dreaming, Assoziation, Brainwriting, Open Innovation, Pitch" />
+
+  <meta property="og:url" content="www.innovationgame.de" />
+  <meta property="og:title" content="Moonshot – The Innovation Game" />
+  <meta property="og:description" content="Innovation lässt sich spielen! Das Innovationsspiel Moonshot macht kreative Prozesse für Unternehmen und Startups spielbar und fördert mit zahlreichen Tools und Challenges ein adaptives Mindset." />
+  <meta property="og:image" content="media/opengraph-image.png" />
+
+  <link rel="shortcut icon" href="media/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="media/apple-touch-icon.png">
+
+  <link rel="shortcut icon" href="media/favicon.ico" />
+  <title>MOONSHOT – The Innovation Game</title>
+
+  <!-- STYLESHEETS -->
+  <link rel="stylesheet" type="text/css" href="stylesheets/screen.css">
+  <link rel="stylesheet" href="https://use.typekit.net/top2kgq.css">
+
+  <!-- JAVSCRIPT -->
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-116548844-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-116548844-1');
+  </script>
+
+</head>
+
+<body>
+  <div id="wrapper">
+    <header>
+      <div class="container">
+        <h1 class="logo">
+          <img src="media/moonshot-logo.svg" alt="Moonshot Logo" title="Moonshot – The Innovation Game">
+          <!-- Hidden -->
+          <span>MOONSHOT – The Innovation Game</span>
+
+        </h1>
+        <div class="line">
+          &nbsp;
+        </div>
+
+        <div class="content">
+          <p class="intro">Innovation lässt sich nicht planen. Es braucht mehr als lineare Methoden, um einen Umgang mit den immer schneller werdenden Veränderungszyklen der Welt finden zu können. Deshalb haben wir das Innovationsspiel <em>Moonshot</em> entwickelt.</p>
+        </div>
+      </div>
+    </header>
+
+    <div class="content-wide content-shadow">
+
+      <div class="container-video">
+        <iframe width="1600" height="900" src="https://www.youtube.com/embed/iA5GyJFrkVc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="content">
+        <h2>Über das Spiel</h2>
+        <p>Moonshot macht den Innovationsprozess spielerisch erfahrbar. Die SpielerInnen treten mit ihren Ideen gegeneinander an und transformieren sie fortwährend entlang von unterschiedlichen <span>Kreativitätstechniken.</span> Das erfordert assoziatives Denken, eine hohe Revisionskompetenz und die Bereitschaft, sich ständig auf wechselnde Rahmenbedingungen einzulassen. So entsteht ein Flirren, das den SpielerInnen radikal neue Perspektiven auf ihre Ideen eröffnet und zuvor Ungesehenes an die Oberfläche wirbelt.</p>
+
+        <p>Ziel von Moonshot ist es nicht, marktreife Produkte zu produzieren, sondern <span>Metakompetenzen</span> zu vermitteln, die für schöpferische Prozesse in komplexen Zeiten unabdingbar sind. Und wenn doch mal das nächste Unicorn dabei ist: umso besser.</p>
+      </div>
+    </div>
+
+    <div class="container container-silver">
+      <div class="content-superwide">
+        <div class="content">
+          <h2 style="text-align: center;">Ein Blick in die Karten</h2>
+        </div>
+
+        <div class="card-container">
+          <div class="card1">
+            <img src="media/moonshot-card-challenge.png" alt="Moonshot Challenge-Karte">
+            <h3>40 Challenge-Karten</h3>
+            <p>Die Challenge-Karten eröffnen in dichten Mikronarrationen überraschende Szenarien und bringen die SpielerInnen dazu, ihre Ideen fortwährend zu transformieren.</p>
+          </div>
+          <div class="card2">
+            <img src="media/moonshot-card-item.png" alt="Moonshot Item-Karte">
+            <h3>60 Item-Karten</h3>
+            <p>Mit den Item-Karten können die SpielerInnen in den Fluss des Spiels eingreifen, sich selbst verstärken und andere SpielerInnen hacken.</p>
+          </div>
+          <div class="card3">
+            <img src="media/moonshot-card-aktion.png" alt="Moonshot Aktions-Karte">
+            <h3>10 Aktions-Karten</h3>
+            <p>Die Aktionskarten sind äußere Störfaktoren, die das Spiel durcheinanderwirbeln und die SpielerInnen aus ihren Routinen herausreißen.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="content content-wide content-contact">
+        <img src="media/rocket.png" alt="Illustration of a rocket">
+        <h2>Kontakt</h2>
+        <p>Du willst Moonshot in deinem Unternehmen spielen? Du willst das Spiel künftig auch in eigenen Workshops verwenden? Du willst privat eine Spielrunde organisieren? Moonshot ist für Konzerne, Start-ups, Agenturen, Beratungen, Coaches und Privatpersonen geeignet. Sag Hi!</p>
+
+        <!-- Encoder: http://robspangler.com/blog/encrypt-mailto-links-to-stop-email-spam/ -->
+        <p class="contact-cta">
+          <a title="Schreib’ uns eine Mail" href="&#x6d;&#97;&#x69;&#x6c;&#116;&#x6f;&#58;&#104;&#101;&#108;&#108;&#111;&#x40;&#116;&#x68;&#101;&#105;&#110;&#110;&#x6f;&#118;&#x61;&#116;&#x69;&#x6f;&#110;&#103;&#x61;&#x6d;&#101;&#46;&#100;&#x65;">Schreib’ uns eine Mail</a>
+        </p>
+      </div>
+    </div>
+
+    <div class="container container-yellow">
+      <div class="content content-wide">
+        <blockquote>
+          <p><span>»The language of entrepreneurs is the language of play. They’re not out there solving problems; they’re out there playing – and it’s during this process that they’re coming up with disruptive technologies and life-changing products.«</span></p>
+          <p class="source">— Bruce Nussbaum</p>
+        </blockquote>
+      </div>
+    </div>
+
+    <div class="container container-violet">
+      <div class="content">
+        <h2>Über uns</h2>
+        <img class="proto-portrait" src="media/martin-wiens-till-hasbach-proto.jpg" alt="Martin Wiens und Till Hasbach von Proto">
+        <p>Wir sind <a href="https://www.linkedin.com/in/till-hasbach/" title="Till Hasbach auf LinkedIn">Till Hasbach</a> und <a href="https://www.linkedin.com/in/martin-wiens/" title="Martin Wiens auf LinkedIn">Martin Wiens</a> und wir entwickeln Spiele für Organisationen. Als Katalysatoren für Beratungs- und Transformationsprozesse erlauben sie es uns, nicht als BeraterInnen im herkömmlichen Sinne agieren zu müssen, sondern als SpielleiterInnen begleitend tätig zu sein.</p>
+
+        <p>Unsere Spiele eröffnen einen Möglichkeitsraum, in dem sich die TeilnehmerInnen ausprobieren können, ohne negative Konsequenzen befürchten zu müssen. Sie sind anstrengend, irritierend und komplex und trainieren so genau das, was die womöglich wichtigste Kompetenz ist, um sich in liquiden Zeiten zurechtzufinden: ein adaptives Mindset.</p>
+      </div>
+    </div>
+
+    <footer>
+      <p>&copy; <?php echo date("Y"); ?> Martin Wiens, Till Hasbach. <a title="Impressum" href="impressum.php">Impressum</a>.</p>
+    </footer>
+
+  </div> <!-- //#wrapper -->
+</body>
+</html>
