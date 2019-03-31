07d6abea80f16073b0e7362957c70dc5e207fe79
christowski <mail@christowski.de>
2019-03-31 22:55:40 +0200

    restyle modal

--- modal.php
+++ modal.php
@@ -4,23 +4,35 @@
 
         <header>
             <span onclick="document.getElementById('formmodal').style.display='none'"
-            class="">CLOSE</span>
+            class="">Close</span>
             <h2>Hol’ Moonshot in dein Unternehmen!</h2>
         </header>
 
           <div class="w3-section">
             <form class="" action="formmailer.php" method="post">
-              <input class="" type="text" placeholder="Name" name="name" required>
-              <input class="" type="mail" placeholder="E-Mail" name="mail" required>
-              <input class="" type="text" placeholder="Organisation" name="organisation" required>
-              <input class="" type="text" placeholder="Straße und Hausnummer" name="street" required>
-              <input class="" type="text" placeholder="Postleitzahl" name="street" required>
-              <input class="" type="text" placeholder="Ort" name="place" required>
+              <input class="" type="text" placeholder="Name *" name="name" required>
+              <input class="" type="mail" placeholder="E-Mail *" name="mail" required>
+              <input class="" type="text" placeholder="Organisation *" name="organisation" required>
+              <input class="" type="text" placeholder="Straße und Hausnummer *" name="street" required>
+              <input class="" type="text" placeholder="Postleitzahl, Ort *" name="street" required>
 
-              <textarea class="" id="subject" type="text" name="subject" placeholder="Möchtest du uns noch etwas sagen?" style="height:100px"></textarea>
+              <textarea class="" id="subject" type="text" name="subject" placeholder="Möchtest du uns noch etwas sagen?" style="height:50px"></textarea>
 
-              <input class="" type="checkbox" required><small>Ich bin damit einverstanden, dass meine Angaben zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden. Hier geht es zur Datenschutzerklärung.</small>
-              <input class="" type="checkbox" required><small>Hiermit möchte ich das Innovationsspiel für zehn Tage ausleihen. Das Angebot ist kostenlos. Den Rückversand übernehmen wir.</small>
+              <div class="modal-checkbox inline-label-form">
+                <div class="field-boxes">
+
+                  <label>
+                    <input class="" type="checkbox" required>
+                    <span>Ich bin damit einverstanden, dass meine Angaben zur Beantwortung meiner Anfrage elektronisch erhoben und gespeichert werden. Hier geht es zur Datenschutzerklärung.</span>
+                  </label>
+
+                  <label>
+                    <input class="" type="checkbox" required>
+                    <span>Hiermit möchte ich das Innovationsspiel für zehn Tage ausleihen. Das Angebot ist kostenlos. Den Rückversand übernehmen wir.</span>
+                  </label>
+
+                </div>
+              </div>
 
               <input type="submit" name="absenden" value="Absenden">
             </form>
