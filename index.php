2fec1622e552ba5968a4687d0468af2ac3a4e321
christowski <mail@christowski.de>
2019-07-01 21:23:55 +0200

    add duo-layout and packshot

--- index.php
+++ index.php
@@ -114,30 +114,48 @@
       </div> <!-- // content-superwide -->
     </div> <!-- // container -->
 
+
+
+
+
     <div class="container container-silver">
-      <div class="content">
-        <h2>Moonshot ausleihen</h2>
-        <p>Spiele lassen sich nicht erzählen. Um sie zu erleben, muss man sie spielen. Hole das Innovationsspiel Moonshot in dein Unternehmen und teste es gemeinsam mit deinen KollegInnen. Dafür musst du das Spiel nicht kaufen und auch keinen Workshop buchen. Du kannst es leihen – ganz einfach und kostenlos.</p>
-        <ul>
-          <li>Erhalte das komplette Paket aus Spielbrett, Challenge-Karten, Item-Karten, Bewertungskarten, Bedienungsanleitung, Drehscheibe, Sanduhr und Knete in dein Unternehmen.</li>
-          <li>Du willst Moonshot mit deinen KollegInnen spielen? Wir schicken dir ein vollständiges Slidedeck, das dir dabei hilft, Moonshot vorzustellen und die Spielregeln sicher zu erklären. Wenn trotzdem noch etwas unklar bleibt, sind wir für dich da.</li>
-          <li>Die Leihdauer beträgt zehn Tage ab dem Zeitpunkt, an dem das Spiel bei dir eintrifft. Die Kosten für den Hinversand übernehmen wir, um den Rückversand kümmerst du dich.</li>
-        </ul>
+      <div class="content-superwide">
+
+        <div class="duo_section--wide">
+
+          <div class="duo_section--one packshot_section--one">
+            <img src="media/moonshot-packshot.jpg" alt="Bild aller Bestandteile des Spiels">
+          </div>
+
+          <div class="duo_section--two packshot_section--two">
+            <h2>Moonshot ausleihen</h2>
+            <p>Spiele lassen sich nicht erzählen. Um sie zu erleben, muss man sie spielen. Hole das Innovationsspiel Moonshot in dein Unternehmen und teste es gemeinsam mit deinen KollegInnen. Dafür musst du das Spiel nicht kaufen und auch keinen Workshop buchen. Du kannst es leihen – ganz einfach und kostenlos.</p>
+            <ul>
+              <li>Erhalte das komplette Paket aus Spielbrett, Challenge-Karten, Item-Karten, Bewertungskarten, Bedienungsanleitung, Drehscheibe, Sanduhr und Knete in dein Unternehmen.</li>
+              <li>Du willst Moonshot mit deinen KollegInnen spielen? Wir schicken dir ein vollständiges Slidedeck, das dir dabei hilft, Moonshot vorzustellen und die Spielregeln sicher zu erklären. Wenn trotzdem noch etwas unklar bleibt, sind wir für dich da.</li>
+              <li>Die Leihdauer beträgt zehn Tage ab dem Zeitpunkt, an dem das Spiel bei dir eintrifft. Die Kosten für den Hinversand übernehmen wir, um den Rückversand kümmerst du dich.</li>
+            </ul>
+
+          </div>
+
+        </div>
 
         <div class="cta-big cta-big--center">
             <a onclick="document.getElementById('formmodal').style.display='block'" class="">Jetzt ausleihen</a>
         </div>
         <?php include 'modal.php' ?>
+
       </div>
     </div>
+
 
     <div class="container container-violet">
       <div class="content content-wide">
         <h2>Unsere Workshop-Formate</h2>
 
-        <div class="plan-container">
+        <div class="duo_section--wide">
 
-          <div class="plan-section1">
+          <div class="duo_section--one plan_section--one">
             <h2 style="text-align: center;">Basic</h2>
             <img src="media/ufo-basic.svg" alt="Illustration of a normal UFO">
             <ul>
@@ -147,13 +165,9 @@
               <li>Dauer: 0,5 Tage</li>
             </ul>
 
-            <!--div class="cta-big">
-              <a title="Call to Action" href="#">Call to Action</a>
-            </div-->
-
           </div>
 
-          <div class="plan-section2">
+          <div class="duo_section--two plan_section--two">
             <h2 style="text-align: center;">Plus</h2>
             <img src="media/ufo-plus.svg" alt="Illustration of a flying UFO">
             <ul>
@@ -162,10 +176,6 @@
               <li>Co-kreative Entwicklung eines individualisierten Innovationsspiels mithilfe unseres Frameworks</li>
               <li>Dauer: 2 bis 3 Tage</li>
             </ul>
-
-            <!--div class="cta-big">
-              <a title="Call to Action" href="#">Call to Action</a>
-            </div-->
 
           </div>
 
