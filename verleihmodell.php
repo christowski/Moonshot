ab80359811badd6013d866442aa437dff9e459f5
christowski <mail@christowski.de>
2019-10-01 21:08:30 +0200

    remove section

---
+++ verleihmodell.php
@@ -0,0 +1,29 @@
+<div class="container container-silver">
+  <div class="content-superwide">
+
+    <div class="duo_section--wide">
+
+      <div class="duo_section--one packshot_section--one">
+        <img src="media/moonshot-packshot.jpg" alt="Bild aller Bestandteile des Spiels">
+      </div>
+
+      <div class="duo_section--two packshot_section--two">
+        <h2>Moonshot ausleihen</h2>
+        <p>Spiele lassen sich nicht erzählen. Um sie zu erleben, muss man sie spielen. Hole das Innovationsspiel Moonshot in dein Unternehmen und teste es gemeinsam mit deinen KollegInnen. Dafür musst du das Spiel nicht kaufen und auch keinen Workshop buchen. Du kannst es leihen – ganz einfach und kostenlos.</p>
+        <ul>
+          <li>Erhalte das komplette Paket aus Spielbrett, Challenge-Karten, Item-Karten, Bewertungskarten, Bedienungsanleitung, Drehscheibe, Sanduhr und Knete in dein Unternehmen.</li>
+          <li>Du willst Moonshot mit deinen KollegInnen spielen? Wir schicken dir ein vollständiges Slidedeck, das dir dabei hilft, Moonshot vorzustellen und die Spielregeln sicher zu erklären. Wenn trotzdem noch etwas unklar bleibt, sind wir für dich da.</li>
+          <li>Die Leihdauer beträgt zehn Tage ab dem Zeitpunkt, an dem das Spiel bei dir eintrifft. Die Kosten für den Hinversand übernehmen wir, um den Rückversand kümmerst du dich.</li>
+        </ul>
+
+      </div>
+
+    </div>
+
+    <div class="cta-big cta-big--center">
+        <a onclick="document.getElementById('formmodal').style.display='block'" class="">Jetzt ausleihen</a>
+    </div>
+    <?php include 'modal.php' ?>
+
+  </div>
+</div>
